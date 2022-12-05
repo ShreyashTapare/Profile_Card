ab18805234d329577132d64b16774653f6b07cd3
shreyashTapare <tapare.shreyash20@gmail.com>
2022-12-05 18:17:29 +0530

    UI Change | Extended Css & Java Script

--- static/src/js/base.js
+++ static/src/js/base.js
@@ -1,2 +1,6 @@
 
 // document.getElementById("addhar_text").innerHTML = "Hello JavaScript";
+
+// function myMoveFunction() {
+//     document.getElementById("Portal_detail_Bg").style.backgroundColor= 'rgb(206, 206, 206)';
+// }
